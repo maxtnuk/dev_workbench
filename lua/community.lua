16bb656b9ae12a9c629fc4c00e359fd71bb166f1
maxtnuk <maxtnuk@gmail.com>
2024-09-23 00:24:59 +0900

    enable bar bar

--- lua/community.lua
+++ lua/community.lua
@@ -8,5 +8,6 @@
 return {
   "AstroNvim/astrocommunity",
   { import = "astrocommunity.pack.lua" },
+  { import = "astrocommunity.recipes.disable-tabline" },
   -- import/override with your plugins folder
 }
