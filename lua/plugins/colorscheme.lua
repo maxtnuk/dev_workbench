db3976cc49100023920dfcf90ee2ffbc4306555f
SH. Song <sh.song@furiosa.ai>
2024-12-01 07:59:55 +0000

    enable some plugins

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,7 +1,9 @@
 ---@type LazySpec
 return {
-  {
-    "EdenEast/nightfox.nvim",
-    config = function(_, _) vim.cmd "colorscheme nightfox" end,
-  },
+	{
+		"EdenEast/nightfox.nvim",
+		config = function(_, _)
+			vim.cmd("colorscheme nightfox")
+		end,
+	},
 }
